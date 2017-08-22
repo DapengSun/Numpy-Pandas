28cad2328d2aecaf89c8e9b112ec58d75c13a83c
Dipa666 <13520387252@163.com>
2017-08-22 21:46:07 +0800

    提交【2017年08月22日】-02

--- testNumpy.py
+++ testNumpy.py
@@ -1,6 +1,8 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plot
 
-array = np.array([[1,2,3],
-                 [4,5,6]])
+data = np.arange([10,20,30])
 
-print array
+plot.plot(data)
+plot.show()
