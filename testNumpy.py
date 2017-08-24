785189569a4362aa0a601cb61e53576faaf8e4e8
13520387252@163.com <13520387252@163.com>
2017-08-24 15:33:56 +0800

    提交【2017年8月24日】

--- testNumpy.py
+++ testNumpy.py
@@ -2,9 +2,20 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import xlrd
+from collections import OrderedDict
+from pyexcel_xls import get_data
 
 try:
-    data  = pd.read_excel(r"E:\PyCharm\Project\Numpy-Pandas\readData.xls", header=0)
+    # date_index = pd.date_range('20170824',periods=6)
+    #
+    # data = pd.DataFrame(pd.arange(),index=date_index,columns=list('ABCD'))
+
+    # for item, in data.iterrows():
+        # print item
+    # print data.shape[0]
+    # print data
+
+    data = np.arange(20)
     print data
 except Exception,ex:
     print ex
