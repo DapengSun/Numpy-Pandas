81677b76d6c37ea398bb9735d35ef19a7e89d7d9
13520387252@163.com <13520387252@163.com>
2017-09-12 23:09:22 +0800

    提交【2017年09月12日】

--- HomeLinkAnalysis.py
+++ HomeLinkAnalysis.py
@@ -17,58 +17,47 @@ db = MySQLdb.connect("localhost", "root", "sdmp", "spiderdb",charset='utf8')
 class Analysis:
     def __init__(self):
         # self.cursor = db.cursor()
-        self.GetDataSql = 'Select * from HouseInfo Where Date(CDate) = "%s"'
+        self.GetDataSql = 'Select HouseName,HouseTotalPrice,HousePrice from HouseInfo'
+    #     ,HousePattern,HouseRange,HousePace,HouseStyle,
 
-    def GetHomeLinkData(self,Date):
+    def GetHomeLinkData(self):
         try:
-            Sql = self.GetDataSql % str(Date)
-            df = pd.read_sql(Sql, db, index_col="ID")
+            # Sql = self.GetDataSql
+            # df = pd.read_sql(Sql, db)
+            # start = time.time()
+            # df['HouseTotalPrice'] = df['HouseTotalPrice'].astype(float)
+            # grouped = df['HouseTotalPrice'].groupby(df['HouseName']).mean()
+            # end = time.time()
+            # grouped.to_excel('foo.xlsx', sheet_name='Sheet1')
+            # print end - start
 
-            # df[['HousePrice']] = df[['HousePrice']].astype('int32')
+            start = time.time()
+            print 'start'
+            reader = pd.read_csv(r'E:\PyCharm\Project\HouseInfo.csv', iterator=True,error_bad_lines=False)
+            loop = True
+            chunkSize = 10000000
+            chunks = []
+            while loop:
+                try:
+                    chunk = reader.get_chunk(chunkSize)
+                    chunks.append(chunk)
+                except StopIteration:
+                    loop = False
+                    print "Iteration is stopped."
+            df = pd.concat(chunks, ignore_index=True)
 
-            # HousePrice = df[['HousePrice']].head(3)
-            # HouseArea = df[['HouseArea']].head(3)
+            df['HouseTotalPrice'] = df['HouseTotalPrice'].astype(float)
+            grouped = df['HouseTotalPrice'].groupby(df['HouseName']).mean()
 
-            HousePrice = ('10000')
-            HouseArea = ('西苑')
+            end = time.time()
+            grouped.to_excel('foo.xlsx', sheet_name='Sheet1')
 
-            bar_width = 0.35
-            index = np.arange(1)
-
-            rects1 = plt.bar(0,HousePrice[0],bar_width,color='#0072BC', label=HouseArea[0])
-            # rects2 = plt.bar(index + bar_width, HousePrice[1], bar_width, color='#0072BC', label=HouseArea[1])
-            # rects3 = plt.bar(index + bar_width + bar_width, HousePrice[2], bar_width, color='#ADFF2F', label=HouseArea[2])
-
-            plt.xticks(index + bar_width, HouseArea)
-            plt.ylim(ymax=200000, ymin=0)# Y轴范围
-            plt.title(u'HomeLink')  # 图表标题
-            plt.show()
-
+            print 'end'
+            print end - start
         except Exception,ex:
             print ex
-        # plt.
-        # plt.ylim(ymax=100, ymin=0)  # Y轴范围
-        #
-        # plt.title(u'HomeLink')  # 图表标题
-        # plt.show()
-        # print df.groupby(['HouseArea'])[['HousePrice']].mean()
-        # df.groupby(['HouseArea'])[['HouseArea','HousePrice']].max().to_excel("abc.xls",sheet_name="123",index=False,header=True)
-        # return df
-        # print df.groupby(['HouseArea'])[['HousePrice']].max()
-        # print df[['HouseArea','HousePrice']].max()
-        # 极差
-        # Range = df['HousePrice'].max() - df['HousePrice'].min()
-        # 组距
-        # GroupLen = 10000
-        # 组数
-        # GroupNum = Range / GroupLen
-        # print GroupNum
-
-    def LocalDate(self):
-        return time.strftime('%Y-%m-%d',time.localtime(time.time()))
 
 _analysis = Analysis()
-LocalDate = _analysis.LocalDate()
-_analysis.GetHomeLinkData(LocalDate)
+_analysis.GetHomeLinkData()
 
 
